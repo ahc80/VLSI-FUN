b8c4f416237c6e624ab14134f46247f32d60775a
Sonara <the5thascender@gmail.com>
2024-09-07 11:05:47 -0400

    editing full_adder.v comment spacing

--- Q1/full_adder.v
+++ Q1/full_adder.v
@@ -10,13 +10,13 @@ module full_adder (
 
     wire L1;        // Intermediate wire for A XOR B
     wire L2;        // Intermediate wire for A AND B
-    wire L3;  // Intermediate wire for (A XOR B) AND Cin
+    wire L3;        // Intermediate wire for (A XOR B) AND Cin
 
     // Structural logic using gates
     xor (L1, A, B);              // XOR gate for A and B
     xor (Sum, L1, Cin);          // XOR gate for (A XOR B) and Cin
     and (L2, A, B);              // AND gate for A and B
-    and (L3, L1, Cin);  // AND gate for (A XOR B) and Cin
-    or  (Cout, L2, L3); // OR gate for carry-out
+    and (L3, L1, Cin);          // AND gate for (A XOR B) and Cin
+    or  (Cout, L2, L3);         // OR gate for carry-out
 
 endmodule

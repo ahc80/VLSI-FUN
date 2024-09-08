fb4d4122ea02a77dc01f29e5096cd3b8713c5c69
Andrew <ahchen169@gmail.com>
2024-09-08 12:25:43 -0400

    Update

---
+++ Q1/concatenator_dynamic.v
@@ -0,0 +1,5 @@
+module concaternator_dynamic (
+    input [1:0]
+);
+    
+endmodule
---
+++ Q1/red_box.v
@@ -0,0 +1,14 @@
+module red_box (
+    input x2, //Going to make the variables based off of the first "red_box"
+    input y2, //But they should apply to all of the "red_box".
+    input x3,
+    input y3,
+    output [1:0] concat_mux1,
+    output mux2,
+    output [1:0] concat_mux3,
+    output mux4,
+);
+
+
+    
+endmodule

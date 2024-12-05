f0448a77cbaf9452f56cd497275838d94f6ee8fc
Sonara <93947986+SonaraSelect@users.noreply.github.com>
2024-12-04 20:44:21 -0500

    calculate levels is a mess... but doesnt crash now

--- Lab5_6/backend/Circuit.java
+++ Lab5_6/backend/Circuit.java
@@ -1,43 +1,49 @@
 package backend;
+
 import java.util.HashMap;
 import java.util.Set;
+
 public class Circuit {
 
     HashMap<String, Wire> wireList;
     // HashMap<String, Gate> gateList; // I dont think we need this class actually?
-                                    // With the wires and inputs we automatically
-                                    // make a graph
-    Wire[] inputs; 
+    // With the wires and inputs we automatically
+    // make a graph
+    Wire[] inputs;
     Wire[] outputs;
     Gate firstGate;
     Gate lastGate;
 
-    Circuit(){
+    Circuit() {
         this.wireList = new HashMap<String, Wire>(27157);
         // this.gateList = new HashMap<String, Gate>(27571);
-        this.inputs   = new Wire[40];  // I counted 38
-        this.outputs  = new Wire[307]; // I counted 304
+        this.inputs = new Wire[40]; // I counted 38
+        this.outputs = new Wire[307]; // I counted 304
     }
 
     /***
      * Adds a wire to the circuit registry. Inputs and outputs to
      * each wire will be taken care of while adding gates
+     * 
      * @param name the name of the wire
      */
-    public void addWire(String name){
+    public void addWire(String name) {
         wireList.put(name, new Wire(name));
     }
 
     /***
      * Adds a gate to the circuit registry. This method will construct
      * a gate given its name and type
+     * 
      * @param name Name of the gate.
-     * @param type backend.GateType ENUM class type. Available types are INPUT, OUTPUT, WIRE, AND, NAND, OR, NOR, NOT, and DFF;
-     * @return Returns the created Gate. Use the returned gate in addFanIn, addFanOut, and addNextGate
+     * @param type backend.GateType ENUM class type. Available types are INPUT,
+     *             OUTPUT, WIRE, AND, NAND, OR, NOR, NOT, and DFF;
+     * @return Returns the created Gate. Use the returned gate in addFanIn,
+     *         addFanOut, and addNextGate
      */
-    public Gate addGate(String name, GateType type){
+    public Gate addGate(String name, GateType type) {
         Gate gate = new Gate(name, type);
-        if(firstGate == null) 
+        if (firstGate == null)
             firstGate = gate;
         lastGate = gate;
         return gate;
@@ -45,21 +51,24 @@ public class Circuit {
 
     /***
      * Adds the output to the gate. In a gate, the leftmost wire is the output
+     * 
      * @param name the name of the wire
      * @param gate the gate you are adding the fanout to
      */
-    public void addFanOut(String name, Gate gate){
+    public void addFanOut(String name, Gate gate) {
         Wire wire = wireList.get(name);
         wire.addInput(gate);
         gate.addFanOut(wire);
     }
 
     /***
-     * Adds the inputs to the gate. In a gate, the 2nd, 3rd, 4th, etc wires are all inputs
+     * Adds the inputs to the gate. In a gate, the 2nd, 3rd, 4th, etc wires are all
+     * inputs
+     * 
      * @param name the name of the wire
      * @param gate the gate you are adding the fanin to
      */
-    public void addFanIn(String name, Gate gate){
+    public void addFanIn(String name, Gate gate) {
         Wire wire = wireList.get(name);
         wire.addOutput(gate);
         gate.addFanIn(wire);
@@ -67,31 +76,33 @@ public class Circuit {
 
     /**
      * Add an input to the circuit registry
+     * 
      * @param name the name of the input
      */
-    public void addInput(String name){
+    public void addInput(String name) {
         Wire wire = new Wire(name, GateType.INPUT);
         wireList.put(name, wire);
         int i = 0;
-        while(inputs[i] != null && i == inputs.length-1){
-            if(i == inputs.length-1)
+        while (inputs[i] != null && i == inputs.length - 1) {
+            if (i == inputs.length - 1)
                 System.err.println("Inputs array max length met");
             i++;
         }
         inputs[i] = wire;
-        wire.addInput(new Gate(name, GateType.INPUT));      // PROBLEM PROBLEM PROBLEM
+        wire.addInput(new Gate(name, GateType.INPUT)); // PROBLEM PROBLEM PROBLEM
     }
 
     /**
      * Add an output wire to the circuit registry
+     * 
      * @param name the name of the output
      */
-    public void addOutput(String name){
+    public void addOutput(String name) {
         Wire wire = new Wire(name, GateType.OUTPUT);
         wireList.put(name, wire);
         int i = 0;
-        while(outputs[i] != null && i == outputs.length-1){
-            if(i == outputs.length-1)
+        while (outputs[i] != null && i == outputs.length - 1) {
+            if (i == outputs.length - 1)
                 System.err.println("Outputs array max length met");
             i++;
         }
@@ -100,29 +111,21 @@ public class Circuit {
     }
 
     /**
-     * Add the next gate to a gate in linked list fashion (helps with parsing for later operations)
+     * Add the next gate to a gate in linked list fashion (helps with parsing for
+     * later operations)
+     * 
      * @param previous the previous gate
-     * @param next the next gate
+     * @param next     the next gate
      */
-    public void addNextGate(Gate previous, Gate next){
+    public void addNextGate(Gate previous, Gate next) {
         previous.nextGate = next;
     }
 
-    public void addBuffers(){
-        // FINISH ME !! FINISH ME !! FINISH ME !! FINISH ME !! FINISH ME !! FINISH ME !! 
-
-        // take all wires
-        // iterate though list
-        //    run create buffer
-        //    delete wire from list
-        //    repeat
-    }
-
-    /* 
-    private void addBufferToList(){
-        // add list of buffers to overall list
-    }
-    */
+    /*
+     * private void addBufferToList(){
+     * // add list of buffers to overall list
+     * }
+     */
 
     /**
      * Prints the details of the circuit gates in a table format.
@@ -131,7 +134,7 @@ public class Circuit {
         if (firstGate != null) {
             // Print the table header
             System.out.printf("%-10s %-10s %-10s %-10s %-20s %-10s %-20s %-10s%n",
-                "GateType", "Output", "GateLevel", "#faninN", "faninWires", "#fanoutM", "fanoutWires", "GateName");
+                    "GateType", "Output", "GateLevel", "#faninN", "faninWires", "#fanoutM", "fanoutWires", "GateName");
 
             // Print details of the first gate
             firstGate.printDetails();
@@ -147,51 +150,55 @@ public class Circuit {
         }
     }
 
-    void createBuffers(){
+    void createBuffers() {
         Set<String> wires = wireList.keySet();
-        
-        for(String wireName : wires){
+
+        for (String wireName : wires) {
             Wire wire = wireList.get(wireName);
             Gate[] firstLastBuf = wire.createBuffers();
             // wireList.remove(wireName);
-            if(firstLastBuf[0] != null) {
+            if (firstLastBuf[0] != null) {
                 this.lastGate.nextGate = firstLastBuf[0];
                 this.lastGate = firstLastBuf[1];
             }
         }
 
-        // wireList.clear();        We should store inputs and outputs in a hashmap!
+        // wireList.clear(); We should store inputs and outputs in a hashmap!
     }
 
-    public void calculateLevels(){
-        for(Wire wire: inputs){
-            if(wire != null){
+    /**
+     * Iterate through all inputs and DFFs to calibrate gate levels
+     */
+    public void calculateLevels() {
+        for (Wire wire : inputs) {
+            if (wire != null) {
+                System.out.println(wire.getLevel());
                 System.out.println("On wire " + wire);
-                wire.calculateLevels(0);
+                wire.calculateLevels(0, new HashMap<String, Entity>());
             }
         }
     }
 
     public static void main(String[] args) {
-        String[] inputs = {"G0","G1","G2","G3"};
-        String[] outputs = {"G17"};
-        String[] wires  = {"G5","G6","G7","G14",
-                           "G8","G12","G15","G16",
-                           "G13","G9","G11","G10"};
-        String[][] gates  = {
-            {"dff", "XG1", "G5", "G10"},
-            {"dff", "XG2", "G6", "G11"},
-            {"dff", "XG3", "G7", "G13"},
-            {"not", "XG4", "G14","G0"},
-            {"and", "XG5", "G8", "G6",  "G14"},
-            {"nor", "XG6", "G12","G7",  "G1"},
-            {"or",  "XG7", "G15","G8",  "G12"},
-            {"or",  "XG8", "G16","G8",  "G3"},
-            {"nor", "XG9", "G13","G12", "G2"},
-            {"nand","XG10","G9", "G15", "G16"},
-            {"nor", "XG11","G11", "G9", "G5"},
-            {"nor", "XG12","G10", "G11","G14"},
-            {"not", "XG13","G17", "G11"}
+        String[] inputs = { "G0", "G1", "G2", "G3" };
+        String[] outputs = { "G17" };
+        String[] wires = { "G5", "G6", "G7", "G14",
+                "G8", "G12", "G15", "G16",
+                "G13", "G9", "G11", "G10" };
+        String[][] gates = {
+                { "dff", "XG1", "G5", "G10" },
+                { "dff", "XG2", "G6", "G11" },
+                { "dff", "XG3", "G7", "G13" },
+                { "not", "XG4", "G14", "G0" },
+                { "and", "XG5", "G8", "G6", "G14" },
+                { "nor", "XG6", "G12", "G7", "G1" },
+                { "or", "XG7", "G15", "G8", "G12" },
+                { "or", "XG8", "G16", "G8", "G3" },
+                { "nor", "XG9", "G13", "G12", "G2" },
+                { "nand", "XG10", "G9", "G15", "G16" },
+                { "nor", "XG11", "G11", "G9", "G5" },
+                { "nor", "XG12", "G10", "G11", "G14" },
+                { "not", "XG13", "G17", "G11" }
         };
 
         Circuit circuit = new Circuit();
@@ -202,8 +209,8 @@ public class Circuit {
         Gate prevGate = null;
         Gate currGate = null;
         // Add gates to list
-        for(i=0;i<gates.length;i++){
-            if(i==0)
+        for (i = 0; i < gates.length; i++) {
+            if (i == 0)
                 prevGate = circuit.parseSingleGate(gates[i]);
             else {
                 currGate = circuit.parseSingleGate(gates[i]);
@@ -212,53 +219,59 @@ public class Circuit {
             }
         }
 
-        System.out.println("----------------------------------------------------------------------------------------------------------");
+        System.out.println(
+                "----------------------------------------------------------------------------------------------------------");
 
         circuit.createBuffers();
         circuit.calculateLevels();
         circuit.printContents();
-        
-        System.out.println("----------------------------------------------------------------------------------------------------------");
+
+        System.out.println(
+                "----------------------------------------------------------------------------------------------------------");
 
     }
 
     /**
      * Converts string "dff" and "nor" etc into GateType format
-     * @param type the desired string type to be changed- 
+     * 
+     * @param type the desired string type to be changed-
      *             does not work with spaces!
      */
-    public static GateType parseType(String type){
+    public static GateType parseType(String type) {
         return GateType.readType(type);
     }
 
     /**
-     * Parses an array of input wire names 
+     * Parses an array of input wire names
      * (will automatically add to wire list and input list)
+     * 
      * @param inputs String array of inputs, format ["G0","G1", etc] NO spaces
      */
-    public void parseInputs(String[] inputs){
-        for(String wire : inputs){
+    public void parseInputs(String[] inputs) {
+        for (String wire : inputs) {
             addInput(wire);
         }
     }
 
     /**
-     * Parses an array of output wire names 
+     * Parses an array of output wire names
      * (will automatically add to wire list and output list)
+     * 
      * @param inputs String array of inputs, format ["G17","G18", etc] NO spaces
      *               also works for an array with only one entry
      */
-    public void parseOutputs(String[] outputs){
-        for(String wire : outputs){
+    public void parseOutputs(String[] outputs) {
+        for (String wire : outputs) {
             addOutput(wire);
         }
     }
 
     /**
      * Parses an array of wires
+     * 
      * @param wires String array of wires, format ["G5",G6",etc] NO spaces
      */
-    public void parseWires(String[] wires){
+    public void parseWires(String[] wires) {
         for (String wire : wires) {
             addWire(wire);
         }
@@ -266,20 +279,21 @@ public class Circuit {
 
     /**
      * Parses a single gate's info and returns the address of the created gate
+     * 
      * @param gateData String array for a single gate
      *                 format [type, name, output, input1, input2, etc] NO spaces
      * @return
      */
-    public Gate parseSingleGate(String[] gateData){
+    public Gate parseSingleGate(String[] gateData) {
         int i;
         Gate gate = null;
-        for(i=1;i<gateData.length;i++){
-            switch (i){
+        for (i = 1; i < gateData.length; i++) {
+            switch (i) {
                 case 1:
                     gate = addGate(gateData[1], parseType(gateData[0]));
                     break;
                 case 2:
-                    addFanOut(gateData[2],gate);
+                    addFanOut(gateData[2], gate);
                     break;
                 default:
                     addFanIn(gateData[i], gate);
--- Lab5_6/backend/Entity.java
+++ Lab5_6/backend/Entity.java
@@ -1,74 +1,77 @@
 package backend;
 
+import java.util.HashMap;
+
 public class Entity {
-    
-    String              name;
-    GateType            type;
+
+    String name;
+    GateType type;
     DataWrapper<Entity> fanIn, fanOut;
-    int                 level;
+    int level;
 
     Entity(String name, GateType type) {
-        this.name   = name;
-        this.type   = type;
-        this.fanIn  = null;
+        this.name = name;
+        this.type = type;
+        this.fanIn = null;
         this.fanOut = null;
-        level       =   -1;
+        level = -1;
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return name;
     }
 
-    String getName(){
+    String getName() {
         return this.name;
     }
 
-    GateType getType(){
+    GateType getType() {
         return this.type;
     }
 
-    int getLevel(){
+    int getLevel() {
         return level;
     }
 
-    void setLevel(int level){
+    void setLevel(int level) {
         this.level = level;
     }
 
-    DataWrapper<Entity> deleteInput(Entity data){
+    DataWrapper<Entity> deleteInput(Entity data) {
         return deleteFromList(data, fanIn);
     }
 
-    DataWrapper<Entity> deleteOutput(Entity data){
+    DataWrapper<Entity> deleteOutput(Entity data) {
         return deleteFromList(data, fanOut);
     }
 
-    private DataWrapper<Entity> deleteFromList(Entity data, DataWrapper<Entity> list){
+    private DataWrapper<Entity> deleteFromList(Entity data, DataWrapper<Entity> list) {
         // Check to see if list is single entry
-        if(list.next == null){
+        if (list.next == null) {
             // Delete single entry
-            if(list.data == data){
+            if (list.data == data) {
                 return null;
-            // Data not in single entry
+                // Data not in single entry
             } else {
                 return list;
             }
-        // Multiple entries in list
+            // Multiple entries in list
         } else {
             DataWrapper<Entity> first = list;
-            DataWrapper<Entity> ptr   = first;
+            DataWrapper<Entity> ptr = first;
             // Check if item in first entry
-            if(list.data == data){
+            if (list.data == data) {
                 return list.next;
             }
             // iterate to data entry before target (or to END of list)
-            while(ptr.next != null && ptr.next.data != data){
+            while (ptr.next != null && ptr.next.data != data) {
                 ptr = ptr.next;
             }
-            // Check to see if next entry is the target (otherwise next entry does not exist)
-            if(ptr.next != null && ptr.next.data == data){
-                if(ptr.next.next != null){
+            // Check to see if next entry is the target (otherwise next entry does not
+            // exist)
+            if (ptr.next != null && ptr.next.data == data) {
+                if (ptr.next.next != null) {
                     ptr.next = ptr.next.next;
                 } else {
                     ptr.next = null;
@@ -77,20 +80,34 @@ public class Entity {
             return first;
         }
     }
-    
+
     // Expects NO intermediary wires are present
     // Dont have to worry about making a DFF list, theyre always in the front
     // Run first with 0 when running on inputs, run with -1 on DFF?
-    void calculateLevels(int level){
-        if(level < this.level){
-            System.out.println("Hit!");
-            this.level = level + 1;
-            DataWrapper<Entity> ptr = this.fanOut;
-            while(ptr != null){
-                if(ptr.data != null)
-                    ptr.data.setLevel(level + 1);
-                ptr = ptr.next;
+    void calculateLevels(int level, HashMap<String, Entity> traversedList) {
+        if (this.level < level) {
+            // WIRES BACKTRACK AND WE DONT ACCOUNT FOR THIS
+
+            System.out.println("Hit on " + getName());
+
+            // If entity is NOT a buffer and has not yet been traversed, increment level and
+            // call recursively
+            if (this.type != GateType.BUF && !traversedList.containsKey(this.getName())) {
+                this.level = level + 1;
+                traversedList.put(this.getName(), this);
+                DataWrapper<Entity> ptr = this.fanOut;
+                while (ptr != null) {
+                    if (ptr.data != null)
+                        ptr.data.calculateLevels(level + 1, traversedList);
+                    ptr = ptr.next;
+                }
+                // We are looking at a buffer or traversed entity
+            } else {
+                // If buffer
+                if (fanOut != null && fanOut.data != null && this.type == GateType.BUF)
+                    fanOut.data.calculateLevels(level, traversedList);
             }
+
         }
     }
 
